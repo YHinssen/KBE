1922eae1e0e7a1b6151dccc258ea1d987c00e9b3
YHinssen <60961038+YHinssen@users.noreply.github.com>
2021-06-21 01:45:29 +0200

    En nog een keer voor de zekerheid

--- KBE/Battery.py
+++ KBE/Battery.py
@@ -7,6 +7,28 @@ from parapy.geom import Position, Point
 
 THIS_DIR = os.path.dirname(__file__)
 
+class Wiring(GeomBase):
+
+    @Part
+    def HighVoltWire(self):
+        return Cylinder(radius=1,
+                        height=400,
+                        angle=90,
+                        position=Position(Point(20,0,0)))
+
+    # @Attribute
+    # def diameter_wire_HV(self):
+    #         diameter = input_wire_thickness_HV
+    #
+    #         wire_thickness = Input(diameter_wire_HV)
+    #
+    # @Part
+    # def LowVoltWire(self):
+    #     @Attribute
+    #     def diameter_wire_LV(self):
+    #         diameter = input_wire_thickness_LV
+    #
+    #     wire_thickness_LV = Input(diameter_wire_LV)
 
 class Battery(GeomBase):
     # The following are the inputs required for this part of the program to run, mostly from the external
@@ -19,6 +41,7 @@ class Battery(GeomBase):
     input_bat_width = Input(20)
     input_bat_height = Input(5)
     input_bat_depth = Input(10)
+    Wirethickness = Input(0.5)
 
     @Attribute
     def Batterywidth(self):
@@ -66,15 +89,30 @@ class Battery(GeomBase):
                    )
 
     @Part
-    def batterythree(self):
-        return Box(quantify=round(self.depth_wingbox / self.Batterydepth),
+    def battery22(self):
+        return Box(quantify=round(self.height_wingbox / self.Batteryheight - 1),
                    width=self.Batterywidth,
                    length=self.Batterydepth,
                    height=self.Batteryheight,
-                   position=translate(Position(Point(0, 0, 0)), 'y', child.index * self.Batterydepth),
+                   position=translate(Position(Point(0, (self.Batterydepth + self.Wirethickness), self.Batteryheight)), 'z', child.index * self.Batteryheight),
                    color='blue',
                    )
 
+    @Part
+    def batterythree(self):
+        return Box(quantify=round(self.depth_wingbox / (self.Batterydepth+self.Wirethickness)),
+                   width=self.Batterywidth,
+                   length=self.Batterydepth,
+                   height=self.Batteryheight,
+                   position=translate(Position(Point(0, 0, 0)), 'y', child.index * (self.Batterydepth + self.Wirethickness)),
+                   color='blue',
+                   )
+
+    @Part
+    def HighVoltWire(self):
+        return Cylinder(radius=1,
+                        height=400,
+                        position=Position(Point(0, 15, 0)))
 
 from parapy.gui import display
 
